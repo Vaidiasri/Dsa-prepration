46eddce96820f885fb144bdbd4c2532a97a07fea
vaibhavghildiyal <vaibhavghildiyal2101@gmail.com>
2026-03-03 19:13:42 +0530

    feat: add bubble sort algorithm implementation

---
+++ day15/Bubble_Sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+print(bubble_sort([64, 34, 25, 12, 22, 11, 90]))
