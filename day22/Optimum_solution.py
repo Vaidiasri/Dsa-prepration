c97ceaf84e5a91f07b80d44f4402aaa6d3bf9482
vaibhavghildiyal <vaibhavghildiyal2101@gmail.com>
2026-03-31 13:54:07 +0530

    optimum solution of the problem

---
+++ day22/Optimum_solution.py
@@ -0,0 +1,9 @@
+def moveZeroes(nums):
+    n = len(nums)
+    
+    i = 0
+    
+    for j in range(n):
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
